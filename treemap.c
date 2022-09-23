860588822a4c44b44d961c491a97e2a10de2da40
Replit user <>
2022-09-23 18:02:04 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -50,10 +50,8 @@ void insertTreeMap(TreeMap * tree, void* key, void * value) {
     while (aux != NULL){
         tree->current= aux;
         if (tree->lower_than(key, aux->pair->key) == 1) aux= aux->left;
-        else if (tree->lower_than(aux->pair->key, key) == 1) aux= aux->left;
+        else if (tree->lower_than(aux->pair->key, key) == 1) aux= aux->right;
         else return;
-      }
-
 }
 
 TreeNode * minimum(TreeNode * x){
