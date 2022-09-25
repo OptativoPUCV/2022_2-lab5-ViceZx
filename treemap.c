9ab22e9ce0eb26b7b99e0ad88d407da15558f5c1
Replit user <>
2022-09-25 15:28:41 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -84,28 +84,40 @@ void removeNode(TreeMap * tree, TreeNode* node) {
   TreeNode* aux = node;
   if (node->left == NULL && node->right == NULL){
     if(node->parent->left == node){
-      node->parent->left= NULL;
+      node->parent->left = NULL;
     }
     if(node->parent->right == node){
-      node->parent->right= NULL;
+      node->parent->right = NULL;
     }
     free(node);
     return;
   }
   if (node->left == NULL && node->right != NULL){
     if (node == node->parent->right){
-      aux= node->right;
-      node->parent->right= aux;
+      aux = node->right;
+      node->parent->right = aux;
     }
     if (node->parent->left == node){
-      aux= node->right;
-      node->parent->left= aux;
+      aux = node->right;
+      node->parent->left = aux;
+    }
+    aux->parent = node->parent;
+    free(node);
+    return;
+  }
+  if (node->left != NULL && node->right == NULL){
+    if (node == node->parent->right){
+      aux = node->left;
+      node->parent->right = aux;
+    }
+    if (node->parent->left == node){
+      aux = node->left;
+      node->parent->left = aux;
     }
     aux->parent= node->parent;
     free(node);
     return;
-    }
-
+  }
 }
 
 void eraseTreeMap(TreeMap * tree, void* key){
